f8735d64926e1b4f79d586bd82e56ccc67ae9ede
csarvesh <56367611+csarvesh@users.noreply.github.com>
2019-10-14 18:54:31 +0530

    Add files via upload

---
+++ PyNewsletter/database.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 13 17:06:59 2019
+
+@author: Sarvesh
+"""
+
+import mysql.connector
+
+mydb = mysql.connector.connect(host = "localhost",user="root",passwd="pass@123",database="sdl")
+mycursor = mydb.cursor()
+
+
+def dbin(u_name,u_email):
+
+    insert="insert into users(name,email) values(%s,%s)"
+    name=[(u_name,u_email)]
+    mycursor.executemany(insert,name)
+    mydb.commit()
+
+
+umails = []
+def dbsel():
+    usmail="select email from users"
+    mycursor.execute(usmail)
+    for mail in mycursor:
+        umails.append(mail[0])
+
+    return umails
+
+def res():
+    res="delete from users where email like '%@%'"
+    mycursor.execute(res)
+    mydb.commit()
---
+++ PyNewsletter/scrap.py
@@ -0,0 +1,69 @@
+import sys
+if sys.version_info[0] < 3:
+    raise Exception("Must be using Python 3")
+
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+def scrappy():
+    my_url = 'https://timesofindia.indiatimes.com/'
+    uClient = uReq(my_url)
+    page_html = uClient.read()
+    uClient.close()
+    Author = '  Created by Sarvesh,Onkar,Eeshani,Fauzia  '
+    filename="news.txt"
+    f=open(filename,'w',encoding="utf-8")
+    
+    page_soup = soup(page_html, "html.parser")
+    print(page_soup.p.text)
+    print("\n\n\n")
+    f.write(page_soup.p.text + "\n\n\n" )
+    containers = page_soup.find_all("div", {"class": "top-story"})
+
+    container = containers[0]
+    contain = container.ul
+    cont = contain.find_all("li")
+    con = cont[0]
+    top = container.h2.text
+    top_n = top.center(80, '#')
+    print(top_n)
+    print('\n')
+    f.write(top_n + '\n')
+    for con in cont:
+        title = con.a["title"]
+        link = con.a["href"]
+        print(title)
+        if link[0] == '/':
+            print(my_url+link[1:])
+            f.write(title + '\t' + my_url+link[1:] + "\n")
+        else:
+            print(link)
+            f.write(title + '\t' + link + "\n")
+        print('\n')
+
+
+    top = page_soup.find("div", {"class": "sechead"})
+    top_n = top.a.text.center(80, '#')
+    print(top_n)
+    containers = page_soup.find_all("div", {"class": "latestNewContainer"})
+    container = containers[0]
+    contain = container.ul
+    cont = contain.find_all("li")
+    con = cont[0]
+    print('\n')
+    for con in cont:
+        title = con.a["title"]
+        link = con.a["href"]
+        print(title)
+        if link[0] == '/':
+            print(my_url+link[1:])
+            f.write(title + '\t' + my_url+link[1:] + "\n")
+        else:
+            print(link)
+            f.write(title + '\t' + link + "\n")
+        print('\n')
+
+    f.close()
+    print('#'*80)
+    print(Author.center(80, '$'))
+    print('\n\n')
